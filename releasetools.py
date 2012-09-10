17e52777c09348a0463d0effc331588a7ef90aa1
mochangming <mochangming@xiaomi.com>
2012-09-10 21:00:48 +0800

    fix releasetools.py for incremental ota
    Change-Id: I96f75551b8aa9c27aa202bfee5cd4714156129d1

--- releasetools.py
+++ releasetools.py
@@ -2,8 +2,6 @@ import common
 import edify_generator
 
 def AddExtraAction(info):
-    common.ZipWriteStr(info.output_zip, "firmware.img", info.input_zip.read("RADIO/firmware.img"))
-    common.ZipWriteStr(info.output_zip, "baseband.img", info.input_zip.read("RADIO/baseband.img"))
     info.script.AppendExtra('ui_print("Update firmware...");');
     info.script.AppendExtra('package_extract_file("firmware.img", "/dev/block/mmcblk0p1");');
     info.script.AppendExtra('ui_print("Update baseband...");');
@@ -15,7 +13,17 @@ def AddExtraAction(info):
     return 
 
 def FullOTA_InstallEnd(info):
+    common.ZipWriteStr(info.output_zip, "firmware.img", info.input_zip.read("RADIO/firmware.img"))
+    common.ZipWriteStr(info.output_zip, "baseband.img", info.input_zip.read("RADIO/baseband.img"))
     AddExtraAction(info)
 
 def IncrementalOTA_InstallEnd(info):
+    source_file = info.source_zip.read("RADIO/firmware.img")
+    target_file = info.target_zip.read("RADIO/firmware.img")
+    if source_file != target_file:
+      common.ZipWriteStr(info.output_zip, "firmware.img", info.target_file.read("RADIO/firmware.img"))
+    source_file = info.source_zip.read("RADIO/baseband.img")
+    target_file = info.target_zip.read("RADIO/baseband.img")
+    if source_file != target_file:
+      common.ZipWriteStr(info.output_zip, "baseband.img", info.target_file.read("RADIO/baseband.img"))
     AddExtraAction(info)
